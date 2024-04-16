1155c94bd2190ddbe67b2f85b4c23ae22bf1dbf1
Mariano Gappa <mariano.gappa@helsing.ai>
2024-04-16 11:03:06 +0100

    Implement solutions up to 78/96.

---
+++ exercises/move_semantics/move_semantics6.rs
@@ -0,0 +1,30 @@
+// move_semantics6.rs
+//
+// You can't change anything except adding or removing references.
+//
+// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
+// for a hint.
+
+fn main() {
+    let data = "Rust is great!".to_string();
+
+    get_char(&data);
+
+    let data_new_owner = string_uppercase(data);
+
+    print!("{}", data_new_owner);
+}
+
+// Should not take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: String) -> String {
+    data = data.to_uppercase();
+
+    println!("{}", data);
+
+    data
+}
